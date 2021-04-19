825313b6a70810a402f62f42fec1af0fe2b72e41
Hannah Patrick <hpatrick@wisc.edu>
2021-04-19 20:52:11 +0000

    build fix

--- DistrictSchoolPerformancePrediction/DistrictSchoolPerformancePrediction/settings.py
+++ DistrictSchoolPerformancePrediction/DistrictSchoolPerformancePrediction/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'behave-django',
+    'behave_django',
 ]
 
 MIDDLEWARE = [
