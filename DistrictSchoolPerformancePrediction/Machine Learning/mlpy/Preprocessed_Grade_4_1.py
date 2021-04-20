71cc8245b04c2915adf6ce08e2c0cccdd9d59166
Hannah Patrick <hpatrick@wisc.edu>
2021-04-20 15:15:13 +0000

    Statement seems to have no effect

--- DistrictSchoolPerformancePrediction/Machine Learning/mlpy/Preprocessed_Grade_4_1.py
+++ DistrictSchoolPerformancePrediction/Machine Learning/mlpy/Preprocessed_Grade_4_1.py
@@ -5,12 +5,12 @@ import pandas as pd
 '''
 
 df = pd.read_excel("Preprocessed_File.xlsx",sheet_name="Sheet3")
-df[df['Raw_Score_8'] == 'None']
+# df[df['Raw_Score_8'] == 'None']
 
 df.drop(['Local ID'],axis = 1,inplace = True)
 
 ##Checking for no. of records which are not useful
-df[(df['Raw_Score_7'] == 'None') & (df['Raw_Score_8'] == 'None') & (df['Raw_Score_9'] == 'None') & (df['Raw_Score_10'] == 'None')]
+# df[(df['Raw_Score_7'] == 'None') & (df['Raw_Score_8'] == 'None') & (df['Raw_Score_9'] == 'None') & (df['Raw_Score_10'] == 'None')]
 
 ## Converting Nones for table seperation
 df['Raw_Score_7'] = df['Raw_Score_7'].replace("None",0)
