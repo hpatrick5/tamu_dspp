37ddf5b711da334d62059887d1283925651ea380
Hannah Patrick <hpatrick@wisc.edu>
2021-04-22 20:47:17 +0000

    Home and about content

--- DistrictSchoolPerformancePrediction/DistrictSchoolPerformancePrediction/settings.py
+++ DistrictSchoolPerformancePrediction/DistrictSchoolPerformancePrediction/settings.py
@@ -24,7 +24,7 @@ AUTH_USER_MODEL = 'auth.User'
 SECRET_KEY = '_uv_lnq5n9#)v9-!&2l-@f2%#c@wonam+-b3iprv)_@91b9-h4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['dspp.herokuapp.com']
 ALLOWED_HOSTS = [
