4ec3f9eb2b25a32de49b14dc05b47477de5295eb
EC2 Default User <ec2-user@ip-172-31-25-114.us-east-2.compute.internal>
2021-04-24 14:45:35 +0000

    Added STATIC_ROOT value needed for heroku build

--- dspp/settings.py
+++ dspp/settings.py
@@ -166,6 +166,7 @@ USE_TZ = True
 ##
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # 'data' is my media folder
