55d2611ebe67e5a241d3ffeb537ae6af9926f0e1
Hannah Patrick <hpatrick@wisc.edu>
2021-04-24 16:42:15 +0000

    initial tests

---
+++ tests/test_user_profile_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class UserStatusViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("status")
+    
+    def test_userstatus_returns_userstatus_template_for_get(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed("user_profile/status.html")
