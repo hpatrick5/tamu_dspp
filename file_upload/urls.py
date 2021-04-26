1ff3589907ac3e13961db40ea77b71ac3c68699c
Hannah Patrick <hpatrick@wisc.edu>
2021-04-26 03:21:45 +0000

    deepsource

--- file_upload/urls.py
+++ file_upload/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^upload$', views.UploadFileView.as_view(), name="upload"),
     url(r'^error', views.ErrorView.as_view(), name="error"),
     url(r'^success', views.SuccessView.as_view(), name="success"),
-    
+
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
--- tests/test_user_registration_apps.py
+++ tests/test_user_registration_apps.py
@@ -7,4 +7,5 @@ from user_registration.apps import UserRegistrationBs4Config
 class UserRegistrationBs4ConfigTest(TestCase):
     def test_userregistrationbs4_config_settings(self):
         self.assertEqual(UserRegistrationBs4Config.name, 'user_registration')
-        self.assertEqual(apps.get_app_config('user_registration').name, 'user_registration')
+        self.assertEqual(
+            apps.get_app_config('user_registration').name, 'user_registration')
