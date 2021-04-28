780bd7a25c0e169ffc1c16d367bda17d732a9f4b
EC2 Default User <ec2-user@ip-172-31-4-171.us-east-2.compute.internal>
2021-04-28 06:53:38 +0000

    successful user file upload 02

--- user_registration/adapter.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from allauth.account.adapter import DefaultAccountAdapter
-
-class MyAccountAdapter(DefaultAccountAdapter):
-
-    def get_login_redirect_url(self, request):
-        path = "/accounts/{username}/"
-        
-        return path.format(username=request.user.username)
