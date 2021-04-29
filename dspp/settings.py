f7461cc422a3e8a7aa6086f1eefdcc703e18d692
Hannah Patrick <hpatrick@wisc.edu>
2021-04-29 02:57:26 +0000

    move sites to top

--- dspp/settings.py
+++ dspp/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'user_registration',
     'bootstrap4',
     'django_thumbs',
