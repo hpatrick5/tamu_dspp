c2cc2162129b02c99ebb84a47b2bf2c975f2a4bf
Hannah Patrick <hpatrick@wisc.edu>
2021-04-29 12:35:11 +0000

    add logging

--- dspp/settings.py
+++ dspp/settings.py
@@ -171,3 +171,36 @@ mimetypes.add_type("text/css", ".css", True)
 # Activate Django-Heroku.
 if DEBUG == False:
     django_heroku.settings(locals(), test_runner=False)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
