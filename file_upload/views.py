61fa28c9ab4bbaa7c3e9bf6fba295de0fb43b51c
Hannah Patrick <hpatrick@wisc.edu>
2021-04-29 04:07:38 +0000

    test

--- file_upload/views.py
+++ file_upload/views.py
@@ -56,7 +56,7 @@ class UploadFileView(TemplateView, LoginRequiredMixin):
             here = os.path.dirname(os.path.abspath(__file__))
             filename = os.path.join(here, 'dummy_ml_model_clf.sav')
             model = pickle.load(open(filename, "rb"))
-            df = pd.read_csv(str(file_path))
+            df = pd.read_csv("/app/media/"+str(file_path))
             X = df.iloc[:, 1:12]
             prediction = model.predict(X)
             pd.DataFrame(prediction).to_csv(filename)
