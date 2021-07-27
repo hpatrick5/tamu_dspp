6a08a039a4fb02d756198eb0cd7c8a858fe9915d
Hannah Patrick <hpatrick@wisc.edu>
2021-07-27 07:41:41 -0500

    changes

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from revproxy.views import ProxyView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
@@ -14,15 +16,16 @@ class AboutView(TemplateView):
     
     
 FILE_MANAGER_URL = "http://54.160.87.107:5000/doc"
-
-class UserFilesView(TemplateView, LoginRequiredMixin):
-    template_name = "account/user_files.html"
+# Doesn't look like we are using the below view
+# @method_decorator(, name='dispatch')
+# class UserFilesView(TemplateView, LoginRequiredMixin):
+#     template_name = "account/user_files.html"
     
-    def get(self, request, *args, **kwargs):
-        # username = request.user_display_user
-        
-        return render(request, self.template_name)
+#     # @login_required(login_url='/accounts/login/')
+#     def get(self, request, *args, **kwargs):
+#         # username = request.user_display_user
+#         return render(request, self.template_name)
 
-class TestProxyView(ProxyView):
-    upstream = 'http://54.160.87.107:5000/doc'
-    
+@method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
+class TestProxyView(ProxyView, LoginRequiredMixin):
+    upstream = FILE_MANAGER_URL
