aab6ca2e9ebeed2b56e0f0723183f34e29e9cb49
Hannah Patrick <hpatrick@wisc.edu>
2021-08-05 16:25:42 -0500

    new subjects, validation, updated teacher template, styling

--- apps/file_upload/forms.py
+++ apps/file_upload/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-SUBJECT_CHOICES = [('READING', 'Reading'), ('MATH', 'Math')]
+SUBJECT_CHOICES = [('READING', 'Reading'), ('MATH', 'Math'), ('WRITING', 'Writing'),
+                   ('SCIENCE', 'Science'), ('SOCIALSTUDIES', 'Social Studies')]
 GRADE_CHOICES = [('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8')]
 
 
