e0b1061ab0d9c69d8e1383f387de25f92b8f129b
Hannah Patrick <hpatrick@wisc.edu>
2021-08-05 13:19:20 -0500

    Model new outputs

--- apps/file_upload/models.py
+++ apps/file_upload/models.py
@@ -20,6 +20,8 @@ def get_trained_file(file, grade, subject):
     file_info = subject.lower() + "_" + grade
     # model_file_path = ('../../ml_models/' + grade+'_'+subject.lower())
     model_file_path = '../../ml_models/model_new'
+    ranges_file_path = '../../ml_models/score_ranges_2021.csv'
+
     filename = os.path.join(here, model_file_path)
     model = pickle.load(open(filename, "rb"))
 
@@ -34,8 +36,45 @@ def get_trained_file(file, grade, subject):
     prediction = model.predict(data)
     output = pd.DataFrame(prediction)
 
+    output[0] = round(output[0])
+
+    ranges_filename = os.path.join(here, ranges_file_path)
+    ranges = pd.read_csv(ranges_filename)
+    index_names = ranges["Grade_Subject"]
+    ranges = ranges.drop("Grade_Subject", axis=1)
+    ranges.index = index_names
+
+    row = ranges.filter(like=file_info, axis=0)
+    thresholds = row.values.tolist()
+
+    distance_to_next_threshold = []
+    distance_to_previous_threshold = []
+
+    mastery_ranges = []
+    for x in output[0]:
+        if x > thresholds[0][2]:
+            mastery_ranges.append("Masters")
+            distance_to_next_threshold.append(0)
+            distance_to_previous_threshold.append(abs(x - thresholds[0][2]))
+        elif thresholds[0][1] < x <= thresholds[0][2]:
+            mastery_ranges.append("Meets")
+            distance_to_next_threshold.append(abs(x - thresholds[0][2]))
+            distance_to_previous_threshold.append(abs(x - thresholds[0][1]))
+        elif thresholds[0][0] < x <= thresholds[0][1]:
+            mastery_ranges.append("Approaches")
+            distance_to_next_threshold.append(abs(x - thresholds[0][1]))
+            distance_to_previous_threshold.append(abs(x - thresholds[0][0]))
+        else:
+            mastery_ranges.append("Did Not Meet")
+            distance_to_next_threshold.append(abs(x - thresholds[0][0]))
+            distance_to_previous_threshold.append(0)
+
     # Adds results as a column
     original_file['Predicted % Score'] = output[0]
+    original_file['Predicted Mastery Range'] = mastery_ranges
+    original_file['% to Next Mastery Range'] = distance_to_next_threshold
+    original_file['% to Previous Mastery Range'] = distance_to_previous_threshold
+
     s_buf = io.StringIO()
     trained_csv = original_file.to_csv(path_or_buf=s_buf, mode="w", header=True)
     
