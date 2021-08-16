e6bb3633988f89fa4763d64e5b0af36a154364c3
Hannah Patrick <hpatrick@wisc.edu>
2021-08-16 17:14:34 -0500

    standardized page names

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$|^home$', views.HomePageView.as_view(), name="home"),
     url(r'^about$', views.AboutView.as_view(), name="about"),
-    url(r'^file_information$', views.FileInformationView.as_view(), name="file_info"),
+    url(r'^file_info$', views.FileInformationView.as_view(), name="file_info"),
     url(r'^download/(?P<path>.*)$', views.TestProxyView.as_view()),
     url(r'^why_use', views.WhyUseView.as_view(), name="why_use"),
     url(r'^results_explained', views.ResultsExplainedView.as_view(), name="results_explained"),
