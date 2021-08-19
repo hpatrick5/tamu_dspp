3a9c21040d507e4b3aecf62a507328c754944c3c
Hannah Patrick <hpatrick@wisc.edu>
2021-08-19 15:52:35 -0600

    dspp storage_backends comment

--- dspp/storage_backends.py
+++ dspp/storage_backends.py
@@ -1,7 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
+# For S3 static file storage
+# See https://testdriven.io/blog/storing-django-static-and-media-files-on-amazon-s3/ for more information
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
-
